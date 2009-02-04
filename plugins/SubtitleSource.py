5260d5e8682330b07f9953d395f554ed76a7b774
patrick.dessalle <patrick.dessalle@9a4b33e3-5948-0410-836e-af33532b7019>
2009-02-04 23:38:15 +0000

    Use the API for SubtitleSource
    git-svn-id: http://periscope.googlecode.com/svn/trunk@22 9a4b33e3-5948-0410-836e-af33532b7019

--- plugins/SubtitleSource.py
+++ plugins/SubtitleSource.py
@@ -16,8 +16,7 @@
 #    along with emesene; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import zipfile, os, urllib2, urllib
-from BeautifulSoup import BeautifulSoup
+import zipfile, os, urllib2, urllib, xml.dom.minidom
 
 import SubtitleDatabase
 
@@ -25,6 +24,9 @@ class SubtitleSource(SubtitleDatabase.SubtitleDB):
 
 	def __init__(self):
 		super(SubtitleSource, self).__init__({"en": "English", "se": "Swedish", "da": "Danish", "fi":"Finnish", "no": "Norwegian", "fr" : "French", "es" : "Spanish"})
+
+		#http://www.subtitlesource.org/api/xmlsearch/Heroes.S03E09.HDTV.XviD-LOL/all/0
+		#http://www.subtitlesource.org/api/xmlsearch/heroes/swedish/0
 
 		self.host = "http://www.subtitlesource.org/"
 		self.search = "sublinks.php?"
@@ -59,20 +61,45 @@ class SubtitleSource(SubtitleDatabase.SubtitleDB):
 	def query(self, token, langs=None):
 		''' makes a query on subtitlessource and returns info (link, lang) about found subtitles'''
 		sublinks = []
-		params = {"q": token}
-		searchurl = self.host + self.search + urllib.urlencode(params)
+		searchurl = "%sapi/xmlsearch/%s/all/0" %(self.host, urllib.quote(token))
 		print "dl'ing %s" %searchurl
 		page = urllib2.urlopen(searchurl)
-		soup = BeautifulSoup(page)
+		xmltree = xml.dom.minidom.parse(page)
+		subs = xmltree.getElementsByTagName("sub")
+
+		for sub in subs:
+			sublang = self.getLG(self.getValue(sub, "language"))
+			if langs and not sublang in langs:
+				continue # The language of this sub is not wanted => Skip
+			dllink = "http://www.subtitlesource.org/download/zip/%s" %self.getValue(sub, "id")
+			print "Link added: %s (%s)" %(dllink,sublang)
+			result = {}
+			result["link"] = dllink
+			result["lang"] = sublang
+			sublinks.append(result)
+		print sublinks
+		'''
 		for subs in soup('a'):
 			sublang = subs("img")[0]["src"].rsplit("/", 1)[1][:-4] #Strip the .gif
 			if langs and not self.getLG(sublang) in langs:
 				continue # The lang of this sub is not wanted => Skip
 			else:
-				dllink = subs["href"]
+				dllink = "http://www.subtitlesource.org/download/zip/id" %id
 				print "Link added: %s (%s)" %(dllink,self.getLG(sublang))
 				result = {}
 				result["link"] = dllink
 				result["lang"] = self.getLG(sublang)
 				sublinks.append(result)
 		return sublinks
+		'''
+		return None
+
+	def getValue(self, sub, tagName):
+		for node in sub.childNodes:
+			if node.nodeType == node.ELEMENT_NODE and node.tagName == tagName:
+				print node.childNodes[0].nodeValue
+				return node.childNodes[0].nodeValue
+
+if __name__ == "__main__":
+	s = SubtitleSource()
+	s.process("/media/disk/Videos/Series/Battlestar Galactica/Battlestar.Galactica.S04E13.HDTV.XviD-0TV.avi", None)
