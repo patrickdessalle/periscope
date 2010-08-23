6a0d66caf347f208c22fdc7c3b7aa619c6ae5ff3
patrick.dessalle <patrick.dessalle@9a4b33e3-5948-0410-836e-af33532b7019>
2010-08-23 19:41:28 +0000

    Try/Except the dir creation
    git-svn-id: http://periscope.googlecode.com/svn/trunk@100 9a4b33e3-5948-0410-836e-af33532b7019

--- bin/setup.py
+++ bin/setup.py
@@ -5,5 +5,8 @@ import os
 PACKAGE = 'periscope-gnome'
 VERSION = '0.1.12'
 
-os.makedirs('debian/python-gnome/usr/lib/nautilus/extensions-2.0/python/')
+try:
+    os.makedirs('debian/python-gnome/usr/lib/nautilus/extensions-2.0/python/')
+except:
+    pass
 shutil.copy('periscope-nautilus/periscope-nautilus.py', 'debian/python-gnome/usr/lib/nautilus/extensions-2.0/python/')
