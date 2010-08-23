65fb7f481574fc3b679e40ee25e14f57624fa4f7
patrick.dessalle <patrick.dessalle@9a4b33e3-5948-0410-836e-af33532b7019>
2010-08-23 19:39:35 +0000

    Fixes to the setup.py for periscope-gnome package
    git-svn-id: http://periscope.googlecode.com/svn/trunk@99 9a4b33e3-5948-0410-836e-af33532b7019

--- bin/setup.py
+++ bin/setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 import shutil
+import os
 
 PACKAGE = 'periscope-gnome'
-VERSION = '0.1.5'
+VERSION = '0.1.12'
 
+os.makedirs('debian/python-gnome/usr/lib/nautilus/extensions-2.0/python/')
 shutil.copy('periscope-nautilus/periscope-nautilus.py', 'debian/python-gnome/usr/lib/nautilus/extensions-2.0/python/')
