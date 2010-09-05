d183477232b2a6bf0ff49e75eba61aaf2d896bcc
patrick.dessalle <patrick.dessalle@9a4b33e3-5948-0410-836e-af33532b7019>
2010-09-05 22:25:41 +0000

    Fix the setup for PPA
    git-svn-id: http://periscope.googlecode.com/svn/trunk@108 9a4b33e3-5948-0410-836e-af33532b7019

--- bin/setup.py
+++ bin/setup.py
@@ -6,14 +6,8 @@ import version
 PACKAGE = 'periscope-gnome'
 VERSION = version.VERSION
 
-setup(name = PACKAGE, version = VERSION,
-      platforms=['all'],
-      license = "GNU LGPL",
-      description = "Use periscope in Nautilus to download subtitles",
-      author = "Patrick Dessalle",
-      author_email = "patrick.dessalle@gmail.com",
-      url = "http://code.google.com/p/periscope/",
-      packages= [ "periscope-nautilus" ],
-      py_modules=["periscope"],
-      scripts = [ "periscope-nautilus.py" ]
-      )
+try:
+	os.makedirs("./debian/periscope-gnome/usr/lib/nautilus/extensions-2.0/python")
+except:
+	pass
+shutil.copy('periscope-nautilus/periscope-nautilus.py', 'debian/periscope-gnome/usr/lib/nautilus/extensions-2.0/python')
